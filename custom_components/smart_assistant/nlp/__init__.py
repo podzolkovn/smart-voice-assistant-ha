fc0347e97ca5827d0c025baab2fea418638140e2
Nikita Podzolkov <n.podzolkov@kompastour.com>
2026-05-09 20:25:32 +0500

    [FIX] command

--- custom_components/smart_assistant/nlp/__init__.py
+++ custom_components/smart_assistant/nlp/__init__.py
@@ -1,3 +1,5 @@
+from .morphology import normalize, normalize_text
+from .fuzzy_matcher import build_search_index, find_device
 from .action_matcher import build_action_index, find_action, split_commands, extract_media_title
 
 __all__ = [
